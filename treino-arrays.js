54022c7ff64f04cceddd3d5460a29167e224d9b1
JNeyNeto <122401966+jneyneto@users.noreply.github.com>
2023-02-02 15:41:04 -0300

    Add files via upload

---
+++ treino-arrays.js
@@ -0,0 +1,55 @@
+//teste .sort
+/* const usuarios = [
+    { nome: "01", id: 23, idade: 13 },
+    { nome: "02", id: 56, idade: 53 },
+    { nome: "03", id: 12, idade: 54 },
+    { nome: "04", id: 6, idade: 15 }
+];
+usuarios.sort((a, b) => {
+    return a.id - b.id;
+});
+
+console.log(usuarios);
+
+usuarios.sort((a, b) => {
+    return b.id - a.id;
+});
+
+console.log(usuarios);
+ */
+
+//teste .sort + localeCompare
+/* const carros = [
+    {
+        nome: "01",
+        marca: "999",
+        ano: 1231,
+        cor: "32"
+    },
+    {
+        nome: "02",
+        marca: "tesla",
+        ano: 1231,
+        cor: "123"
+    }, {
+        nome: "03",
+        marca: "BMW",
+        ano: 1231,
+        cor: "4234"
+    }, {
+        nome: "04",
+        marca: "fiat",
+        ano: 1231,
+        cor: "1"
+    },
+];
+
+function ordenarCarros(array, chave) {
+    array.sort((a, b) => {
+        return a[chave].localeCompare(b[chave]);
+    });
+    console.log(array);
+}
+ordenarCarros(carros, "cor")
+ */
+
